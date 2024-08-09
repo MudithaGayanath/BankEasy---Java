cc448058cff0f75fa3b69fb8755f76e873620976
Muditha Gayanath <mudithawijesigha@gmail.com>
2024-08-09 17:51:19 +0530

    load pro,dis and city. not complete yet.

--- src/GUI/CreateAccount2.java
+++ src/GUI/CreateAccount2.java
@@ -13,29 +13,28 @@ import java.util.logging.Logger;
 
 public class CreateAccount2 extends javax.swing.JFrame {
 
-    public HashMap<String,String> userData;
-    
+    public HashMap<String, String> userData;
 
     public CreateAccount2(HashMap map) {
         initComponents();
-        
+
         subHeading.requestFocusInWindow();
         this.userData = map;
         String val = userData.get("line1");
-        if ( val != null ){
+        if (val != null) {
             setup();
         }
         try {
             loadPros();
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(this, "Unable to load provinces");
+            JOptionPane.showMessageDialog(this, "Unable to load Province");
         } catch (ClassNotFoundException ex) {
             JOptionPane.showMessageDialog(this, "Error");
         }
-        
+
     }
-    
-    private void setup(){
+
+    private void setup() {
         line1.setText(String.valueOf(userData.get("line1")));
         line2.setText(String.valueOf(userData.get("line2")));
         pro.setSelectedItem(userData.get("pro"));
@@ -43,17 +42,35 @@ public class CreateAccount2 extends javax.swing.JFrame {
         city.setSelectedItem(userData.get("city"));
         zipCoed.setText(String.valueOf(userData.get("zipCode")));
     }
-    
-    private void loadPros() throws SQLException,ClassNotFoundException {
-        ResultSet rs =  Mysql.search("SELECT * FROM `province`");
+
+    private void loadPros() throws SQLException, ClassNotFoundException {
+        ResultSet rs = Mysql.search("SELECT * FROM `province`");
         Vector<String> pros = new Vector<>();
-        while (rs.next()){
+        pros.add("Province");
+        while (rs.next()) {
             pros.add(rs.getString("province_name"));
         }
         pro.setModel(new DefaultComboBoxModel(pros));
-        
-        
+    }
 
+    private void loadDistrict(int index) throws SQLException, ClassNotFoundException {
+        ResultSet rs = Mysql.search("SELECT * FROM `district` WHERE `province_id`='"+index+"'");
+        Vector<String> dis = new Vector<>();
+        dis.add("District");
+        while (rs.next()) {
+            dis.add(rs.getString("district_name"));
+        }
+        distric.setModel(new DefaultComboBoxModel(dis));
+    }
+
+    private void loadCity(int index) throws SQLException, ClassNotFoundException {
+        ResultSet rs = Mysql.search("SELECT * FROM `city` WHERE `district_id`='"+index+"'");
+        Vector<String> cty = new Vector<>();
+        cty.add("City");
+        while (rs.next()) {
+            cty.add(rs.getString("city_name"));
+        }
+        city.setModel(new DefaultComboBoxModel(cty));
     }
 
     @SuppressWarnings("unchecked")
@@ -112,16 +129,30 @@ public class CreateAccount2 extends javax.swing.JFrame {
         });
 
         pro.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
-        pro.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Westen Province", "Central Province", "Sabaragamuwa Province" }));
+        pro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                proActionPerformed(evt);
+            }
+        });
 
         distric.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
-        distric.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Kandy", "Kegalle", "colombo" }));
+        distric.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "District" }));
+        distric.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                districActionPerformed(evt);
+            }
+        });
 
         city.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
-        city.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "kegalle", "Deraniyagala", "Kandy", "peradeniya" }));
+        city.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "City" }));
+        city.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cityActionPerformed(evt);
+            }
+        });
 
+        zipCoed.setEditable(false);
         zipCoed.setFont(new java.awt.Font("Calibri", 0, 18)); // NOI18N
-        zipCoed.setText("Zip Code");
         zipCoed.addFocusListener(new java.awt.event.FocusAdapter() {
             public void focusGained(java.awt.event.FocusEvent evt) {
                 zipCoedFocusGained(evt);
@@ -162,16 +193,18 @@ public class CreateAccount2 extends javax.swing.JFrame {
                         .addComponent(subHeading, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(back, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(back, javax.swing.GroupLayout.PREFERRED_SIZE, 213, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(pro, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(city, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(city, 0, 225, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(zipCoed)
-                                .addComponent(distric, 0, 180, Short.MAX_VALUE))
-                            .addComponent(next, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(next, javax.swing.GroupLayout.DEFAULT_SIZE, 204, Short.MAX_VALUE)
+                                .addGap(12, 12, 12))
+                            .addComponent(distric, javax.swing.GroupLayout.Alignment.TRAILING, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(zipCoed, javax.swing.GroupLayout.Alignment.TRAILING))))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -195,9 +228,9 @@ public class CreateAccount2 extends javax.swing.JFrame {
                     .addComponent(city, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
                     .addComponent(zipCoed))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(next, javax.swing.GroupLayout.DEFAULT_SIZE, 50, Short.MAX_VALUE)
-                    .addComponent(back, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(back, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(next, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(0, 23, Short.MAX_VALUE))
         );
 
@@ -226,20 +259,21 @@ public class CreateAccount2 extends javax.swing.JFrame {
     }//GEN-LAST:event_backActionPerformed
 
     private void nextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nextActionPerformed
-        if( validFilds()){}else{
-            userData.put("line1",line1.getText());
-            userData.put("line2",line2.getText());
-            userData.put("pro",String.valueOf(pro.getSelectedItem()));
-            userData.put("distric",String.valueOf(distric.getSelectedItem()));
-            userData.put("city",String.valueOf(city.getSelectedItem()));
-            userData.put("zipCode",zipCoed.getText());
+        if (validFilds()) {
+        } else {
+            userData.put("line1", line1.getText());
+            userData.put("line2", line2.getText());
+            userData.put("pro", String.valueOf(pro.getSelectedItem()));
+            userData.put("distric", String.valueOf(distric.getSelectedItem()));
+            userData.put("city", String.valueOf(city.getSelectedItem()));
+            userData.put("zipCode", zipCoed.getText());
             new CreateAccount3(userData).setVisible(true);
             this.dispose();
         }
     }//GEN-LAST:event_nextActionPerformed
 
     private void line1FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_line1FocusGained
-         if (line1.getText().equals("Line 1")) {
+        if (line1.getText().equals("Line 1")) {
             line1.setText("");
         } else {
             line1.setText(line1.getText());
@@ -247,7 +281,7 @@ public class CreateAccount2 extends javax.swing.JFrame {
     }//GEN-LAST:event_line1FocusGained
 
     private void line1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_line1FocusLost
-         if (line1.getText().isEmpty()|| line1.getText().equals(" ")) {
+        if (line1.getText().isEmpty() || line1.getText().equals(" ")) {
             line1.setText("Line 1");
         } else {
             line1.setText(line1.getText());
@@ -255,7 +289,7 @@ public class CreateAccount2 extends javax.swing.JFrame {
     }//GEN-LAST:event_line1FocusLost
 
     private void line2FocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_line2FocusGained
-        if (line2.getText().equals("Line 2") ) {
+        if (line2.getText().equals("Line 2")) {
             line2.setText("");
         } else {
             line2.setText(line2.getText());
@@ -263,7 +297,7 @@ public class CreateAccount2 extends javax.swing.JFrame {
     }//GEN-LAST:event_line2FocusGained
 
     private void line2FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_line2FocusLost
-         if (line2.getText().isEmpty() || line2.getText().equals(" ")) {
+        if (line2.getText().isEmpty() || line2.getText().equals(" ")) {
             line2.setText("Line 2");
         } else {
             line2.setText(line2.getText());
@@ -271,7 +305,7 @@ public class CreateAccount2 extends javax.swing.JFrame {
     }//GEN-LAST:event_line2FocusLost
 
     private void zipCoedFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_zipCoedFocusGained
-          if (zipCoed.getText().equals("Zip Code") ) {
+        if (zipCoed.getText().equals("Zip Code")) {
             zipCoed.setText("");
         } else {
             zipCoed.setText(zipCoed.getText());
@@ -285,22 +319,45 @@ public class CreateAccount2 extends javax.swing.JFrame {
             zipCoed.setText(zipCoed.getText());
         }
     }//GEN-LAST:event_zipCoedFocusLost
+
+    private void proActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_proActionPerformed
+        try {
+            loadDistrict(pro.getSelectedIndex());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, "Unable to load district");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(this, "Error");
+        }
+    }//GEN-LAST:event_proActionPerformed
+
+    private void districActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_districActionPerformed
+        try {
+            loadCity(distric.getSelectedIndex());
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(this, "Unable to load city");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(this, "Error");
+        }
+    }//GEN-LAST:event_districActionPerformed
+
+    private void cityActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cityActionPerformed
+        ResultSet rs = Mysql.search("SELECT * FROM `city` WHERE `zip_code`='"+city.getS+"'");
+    }//GEN-LAST:event_cityActionPerformed
     private boolean validFilds() {
         if (line1.getText().equals("Line 1") || line1.getText().isEmpty()) {
             JOptionPane.showMessageDialog(this, "Line 1 requerd");
             line1.grabFocus();
-            return  true;
+            return true;
         } else if (line2.getText().equals("Line 2") || line2.getText().isEmpty()) {
             JOptionPane.showMessageDialog(this, "Line 2 requerd");
             line2.grabFocus();
-            return  true;
-        }else if ( zipCoed.getText().isEmpty() || zipCoed.getText() == " " || zipCoed.getText().equals("Zip Code")){
+            return true;
+        } else if (zipCoed.getText().isEmpty() || zipCoed.getText() == " " || zipCoed.getText().equals("Zip Code")) {
             JOptionPane.showMessageDialog(this, "Zip Code requerd");
             zipCoed.grabFocus();
-            return  true;
-        }
-        else{
-            return  false;
+            return true;
+        } else {
+            return false;
         }
     }
 
